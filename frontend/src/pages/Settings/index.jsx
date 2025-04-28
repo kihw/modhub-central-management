4a115f419d295db372cbcd5a7c3aa7cc47659e63
kihw <kihwouih@gmail.com>
2025-04-28 12:34:50 +0200

    &

--- frontend/src/pages/Settings/index.jsx
+++
@@ -1,8 +0,0 @@
-```jsx
-import React, { useState, useEffect } from 'react';
-import { FiSettings, FiSave, FiRefreshCw, FiHelpCircle, FiToggleLeft, FiToggleRight } from 'react-icons/fi';
-import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
-import { Switch, Select, Input, Button, notification, Tooltip, Card, Divider } from '../../components/ui';
-import { useSettings } from '../../contexts/SettingsContext';
-import { useTheme } from '../../contexts/ThemeContext';
-import ConfirmDialog from '../../components/ConfirmDialog';
